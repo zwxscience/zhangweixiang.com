0716934d3d6132b0c9ad8905f32f9a1db384e3a5
xavier <zwxscience@gmail.com>
2018-10-10 18:07:01 +0800

    Add files via upload

---
+++ js/updateResult.js
@@ -0,0 +1,18 @@
+setTimeout(function() {
+	var queryId = window.location.pathname.indexOf('?');
+	var mainPath = queryId >= 0 ? window.location.pathname.slice(0, queryId) : window.location.pathname;
+	mainPath = mainPath == "/" ? window.location.href : mainPath;//frontPage
+	var title = ($(document).attr("title") == null) ? $('title').text() : $(document).attr("title");
+		 $.ajax({
+		     url: '../visitinfo.ashx?url=' + mainPath + '&from=mysite&title=' + $.trim(title),
+			dataType: 'json',
+			timeout: 1000 * 3, // 3 sec
+			success: function(data) {
+				console.log('update site visit result:'+data);
+			},
+			error: function(xhr,status,error) {
+				// if fail to get up-to-date data from mysite, get cached local version
+				console.log('Failed to get page view from my site!');	
+			}
+		});	
+        }, 3000);
