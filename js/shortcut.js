6bbc388b3965e68957737254c10532dcbffc1934
xavier <zwxscience@gmail.com>
2018-10-12 19:00:18 +0800

    Update shortcut.js

--- js/shortcut.js
+++ js/shortcut.js
@@ -5,7 +5,7 @@ var shortcut = [
 [2, "我的Github", "images/icos/github.png", "https://github.com/zwxscience", 1000, 700],
 [3, "我的微博", "images/icos/weibo.png", "http://www.weibo.com/weixiazh", 1000, 700],
 [4, "我的豆瓣", "images/icos/douban.png", "http://www.douban.com/people/48750803/", 1000, 700],
-[5,"我的微信","images/icos/tencent_weixin.png","http://zhangweixiang.com/cv/img/weixiangzhang.jpg",600,600],
+[5,"我的微信","images/icos/tencent_weixin.png","https://cv.zhangweixiang.com/img/weixiangzhang.jpg",600,600],
 [6, "百度云", "images/icos/baidu.png", "http://pan.baidu.com/disk/home", 1000, 700],
 [7, "我的音乐", "images/icos/03.png", "http://music.163.com/outchain/player?type=0&id=152261&auto=0&height=430", 500, 700],
 [8, "二维码生成", "images/icos/qr.png", "http://zhangweixiang.com/windows/qrcode.html", 1000, 700],
